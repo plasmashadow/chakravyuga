9c1452db5791c73ae717ab7d47a4aacaf213e573
plasmashadow <plasmashadowx@gmail.com>
2015-06-27 14:40:13 +0530

    adding task and resources

--- test/test.js
+++ test/test.js
@@ -1,34 +1,23 @@
 var assert = require('assert');
 var charkra = require('../index.js');
+var taskobserver = require('../lib/Tashhash.js');
+var resobserver = require('../lib/Resourcehash.js');
 
-describe('selecter suite', function(){
 
-	var test_data = [{
-		id: 1,
-		activity: 1,
-		skill: 2
-	},{
-		id: 2,
-		activity: 1,
-		skill: 3
-	},{
-		id: 3,
-		activity: 2,
-		skill: 3
-	},{
-		id: 4,
-		activity: 1,
-		skill: 3
-	}]
-   
-    it('should select resource properly', function(){
-    	// var resource = chakra.select(test_data, 3);
-    	// var flag = false;
-    	// if(resource.id == 2 || resource.id == 4){
-    	//     flag = true;
-    	// }
-    	// assert.ok(flag);
-    	assert.ok(true);
-    });
+describe('taskobserver suite', function(){
 
+	it('should add the task', function(){
+		taskobserver.addTask("hello", {'a': 1});
+		var value = taskobserver.get("hello");
+		assert.ok(value[0].task['a'], 1);
+	});
+});
+
+describe('resouce observer suite', function(){
+
+	it('should add observer', function(){
+		resobserver.addResource("hello", {"hey": 1});
+		var value = resobserver.get("hello");
+		assert.ok(value[0].resource['hey'], 1);
+	})
 })
